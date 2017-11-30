9b42302d551226624132f887833e10fa986106cb
ds <ds@idvlap015.idvnet.local>
2017-11-30 15:33:28 +0100

    Initial Commit

---
+++ GitTest/src/de/streubel/test/main/MainApp.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package de.streubel.test.main;
+
+/**
+ * @author ds
+ *
+ */
+public class MainApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
