124776f749eb2418f8d679fafc308baf8cd6fde1
ds <ds@idvlap015.idvnet.local>
2017-12-04 08:35:47 +0100

    new method: run()

--- GitTest/src/de/streubel/test/main/MainApp.java
+++ GitTest/src/de/streubel/test/main/MainApp.java
@@ -13,8 +13,13 @@ public class MainApp {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		MainApp mainApp = new MainApp();
+		mainApp.run();
 
+	}
+	
+	private void run() {
+		System.out.println("App is running");
 	}
 
 }
