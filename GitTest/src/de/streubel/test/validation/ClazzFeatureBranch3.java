cc5ac2af91c54b4477b5d1731bc4ecb834a161aa
Dieter Streubel <Dieter.Streubel@lv1871.de>
2017-12-05 13:24:03 +0100

    added attribute test

--- GitTest/src/de/streubel/test/validation/ClazzFeatureBranch3.java
+++ GitTest/src/de/streubel/test/validation/ClazzFeatureBranch3.java
@@ -3,5 +3,6 @@ package de.streubel.test.validation;
 import java.math.BigDecimal;
 
 public class ClazzFeatureBranch3 {
-	private BigDecimal dec; 
+	private BigDecimal dec;
+	private String test;
 }
